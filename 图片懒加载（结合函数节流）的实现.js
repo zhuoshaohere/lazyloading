312985ceb7c18951bf75ebbbfb590fd3141e90d7
zhuoshaohere <835500170@qq.com>
2018-03-16 10:21:48 +0800

    Update 图片懒加载（结合函数节流）的实现.js

--- 图片懒加载（结合函数节流）的实现.js
+++ 图片懒加载（结合函数节流）的实现.js
@@ -22,31 +22,35 @@ class DealImage{
        var img_array = this.image_url;
         for(var i = num*20;i < (this.len - i*20 <= 20?this.len:(num+1)*20);i++){
             /*以下载入图片*/
-            var img_index = this[i];
-            var url = img_array[img_index];
-            //var min_height = Math.min(...this.minHeight);
-            var min_height = Math.min.apply(Math,this.minHeight);
-            var min_index = this.minHeight.indexOf(min_height);
+            var img_index = this[i],
+                url = img_array[img_index],           
+                min_height = Math.min(...this.minHeight),
+                min_index = this.minHeight.indexOf(min_height);
             //事先定义好类
             var load_ele = $("ul:eq(min_index) .prepare_load:eq(0)")[0];
-            load_ele.classList.remove("prepare_load");//加载后清除待加载标记
-            load_ele.src = url;
+                load_ele.classList.remove("prepare_load");//加载后清除待加载标记
+                load_ele.src = url;
             /*更新位置数组*/
             this.minHeight[min_index]+=load_ele.offsetHeight;
         }
+        this.updateNum(); //更新num,为下次加载做准备
+    }
+    //初始加载
+    initLoad = function(){
+    this.loadImage(0);
     }
 }
+/*以下为具体实施方法*/
 var myDealImage = new DealImage([]);
 //页面初始化，先加载20张
-myDealImage.loadImage(0);
-myDealImage.updateNum();
+myDealImage.initLoad();
 //等待resize 或 scroll事件触发以更新图片
 $(document).on("scroll resize",function(){
     //先判断是否满足加载条件（滚动条的高度位置+可见窗口大小超过了高度最低的图片）
     //var min_height = Math.min(...myDealImage.minHeight);
-    var min_height = Math.min.apply(Math,this.minHeight);
+    var min_height = Math.min.apply(Math,myDealImage.minHeight);
     var documentEle = document.documentElement;
     if(documentEle.scrollTop+ documentEle.clientHeight >= min_height) {
-        myDealImage.Throttle(myDealImage.loadImage);
+        myDealImage.Throttle(myDealImage.loadImage(myDealImage.num));
     }
 });
